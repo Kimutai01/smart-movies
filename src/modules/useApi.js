305b82d49b59a454e0e118426519e422a4e0c546
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-06-20 22:01:06 +0100

    display data

--- src/modules/useApi.js
+++ src/modules/useApi.js
@@ -1,10 +1,12 @@
+import displayData from './displayData.js';
+
 const getData = async () => {
   const response = await fetch('https://api.tvmaze.com/shows');
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
   const data = await response.json();
-  return data;
+  displayData(data);
 };
 
 export default getData;
